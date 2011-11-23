936a3d3bffd5f1d00f70be6581c533a697b86b22
Jiri Knesl <jiri.knesl@gmail.com>
2011-11-23 07:21:11 +0100

    add kdev_test

---
+++ Kdev_Test.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Tests base class
+ *
+ * @author Jiri Knesl
+**/
+abstract class KDev_Test extends PHPUnit_Framework_TestCase
+{
+	protected $object;
+	private $__mocks = array();
+
+	/**
+	 * 
+	 */
+	protected function setUp()
+	{
+		$this->cleanup();
+		$this->__mocks = array();
+		$this->prepare();
+	}
+
+	function tearDown()
+	{
+		$this->assertMocks();
+		$this->cleanup();
+	}
+
+	function assertMocks()
+	{
+		foreach ($this->__mocks as $mock) {
+			$mock->assertExpectations();
+		}
+	}
+
+	function prepare()
+	{
+	}
+
+	function cleanup()
+	{
+	}
+
+	function __get($name)
+	{
+		if (0 === strpos($name, "mock")) {
+			$mock = $this->$name();
+			$mock->freeze();
+			$this->__mocks[] = $mock;
+			return $mock;
+		}
+	}
+
+}
