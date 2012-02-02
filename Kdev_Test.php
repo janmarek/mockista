3b2c78f58088874b7f4cd775b3d26961de3eec21
Jiri Knesl <jiri.knesl@gmail.com>
2012-02-02 22:48:20 +0100

    fixes

--- Kdev_Test.php
+++ Kdev_Test.php
@@ -1,54 +1,56 @@
 <?php
-
-/**
- * Tests base class
- *
- * @author Jiri Knesl
-**/
-abstract class KDev_Test extends PHPUnit_Framework_TestCase
-{
-	protected $object;
-	private $__mocks = array();
+if (! class_exists("KDev_Test")) {
 
 	/**
-	 * 
-	 */
-	protected function setUp()
+	 * Tests base class
+	 *
+	 * @author Jiri Knesl
+	**/
+	abstract class KDev_Test extends PHPUnit_Framework_TestCase
 	{
-		$this->cleanup();
-		$this->__mocks = array();
-		$this->prepare();
-	}
+		protected $object;
+		private $__mocks = array();
 
-	function tearDown()
-	{
-		$this->assertMocks();
-		$this->cleanup();
-	}
-
-	function assertMocks()
-	{
-		foreach ($this->__mocks as $mock) {
-			$mock->assertExpectations();
+		/**
+		 * 
+		 */
+		protected function setUp()
+		{
+			$this->cleanup();
+			$this->__mocks = array();
+			$this->prepare();
 		}
-	}
 
-	function prepare()
-	{
-	}
-
-	function cleanup()
-	{
-	}
-
-	function __get($name)
-	{
-		if (0 === strpos($name, "mock")) {
-			$mock = $this->$name();
-			$mock->freeze();
-			$this->__mocks[] = $mock;
-			return $mock;
+		function tearDown()
+		{
+			$this->assertMocks();
+			$this->cleanup();
 		}
-	}
 
+		function assertMocks()
+		{
+			foreach ($this->__mocks as $mock) {
+				$mock->assertExpectations();
+			}
+		}
+
+		function prepare()
+		{
+		}
+
+		function cleanup()
+		{
+		}
+
+		function __get($name)
+		{
+			if (0 === strpos($name, "mock")) {
+				$mock = $this->$name();
+				$mock->freeze();
+				$this->__mocks[] = $mock;
+				return $mock;
+			}
+		}
+
+	}
 }
