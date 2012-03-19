b63ac88e755b24aeb6c275f4da690da22f0ca65b
Jiri Knesl <jiri.knesl@gmail.com>
2012-03-19 08:51:57 +0100

    add lazy proxy generator
    --HG--
    extra : rebase_source : bc3931d9ac6357f3ea61b757c63f069e7d66ffc8

---
+++ LazyProxyGeneratorTest.php
@@ -0,0 +1,50 @@
+<?php
+require_once "bootstrap.php";
+
+class LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class
+{
+	public static $constructorCalled = false;
+	private $x;
+
+	public function __construct($x)
+	{
+		self::$constructorCalled = true;
+		$this->x = $x;
+	}
+
+	public function x()
+	{
+		return $this->x;
+	}
+}
+
+
+/**
+ *
+ * @author Jiri Knesl
+**/
+class LazyProxyGeneratorTest extends KDev_Test
+{
+	/**
+	 * 
+	 */
+	public function prepare()
+	{
+		$this->object = new Mockista\LazyProxyGenerator;
+		$this->object->setMethodFinder(new Mockista\MethodFinder);
+	}
+
+	/**
+	 * 
+	 */
+	public function testGenerateProxy()
+	{
+		$obj = $this->object->generate("LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class", "LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class_Inherited");
+		eval($obj);	
+		$proxy = new LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class_Inherited(33);
+		$this->assertFalse(LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class::$constructorCalled);
+		$ret = $proxy->x();
+		$this->assertEquals(33, $ret);
+		$this->assertTrue(LazyProxyGeneratorTest_Auxiliary_Lazy_Loaded_Class::$constructorCalled);
+	}
+}
