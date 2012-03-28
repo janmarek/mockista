bbc89796ac4e624965b014fb21c802c00c46b9ed
Jiri Knesl <jiri.knesl@gmail.com>
2012-03-28 15:29:17 +0200

    fix require for linux

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__ . "/MockistaInterface.php";
 require_once __DIR__ . "/Mockista.php";
-require_once __DIR__ . "/KDev_Test.php";
+require_once __DIR__ . "/Kdev_Test.php";
 require_once __DIR__ . "/Expect.php";
